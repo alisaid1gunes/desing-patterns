eb2629a840a04ac37b876407f4f04d72353cb407
alisaidgunes <alisaidgunes1@gmail.com>
2023-06-14 18:46:11 +0300

    Update: Singleton Pattern Example

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println("s1 == s2 : " + (s1 == s2));
+
+        SingletonLazy s3 = SingletonLazy.getInstance();
+        SingletonLazy s4 = SingletonLazy.getInstance();
+        System.out.println("s3 == s4 : " + (s3 == s4));
     }
 }
