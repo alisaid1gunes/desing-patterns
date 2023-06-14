dfada55ccbe51d21373287b72bf2e07a6d33c80d
alisaidgunes <alisaidgunes1@gmail.com>
2023-06-14 18:53:00 +0300

    Update: Adapter Pattern Example

--- Adapter/src/Main.java
+++ Adapter/src/Main.java
@@ -1,4 +1,6 @@
 public class Main {
+    //Adapter pattern lets you wrap an otherwise incompatible object in an adapter to make it compatible with another class.
+    //It is a structural pattern as this pattern acts as a wrapper to existing class.
     public static void main(String[] args) {
         TypeC typeC = new TypeC();
         Cable cable = new CableAdapter(typeC);
