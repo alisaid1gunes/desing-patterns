f145c595c447f95d1f976264e5634de78fe9f792
alisaidgunes <alisaidgunes1@gmail.com>
2023-06-14 18:48:11 +0300

    Update: Singleton Pattern Example

--- Singleton/src/Main.java
+++ Singleton/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        //Singleton pattern is used to create only one instance of an object and provide global access to it.
+        //It is a creational pattern as this pattern provides a way to instantiate an object while making sure that only single object gets created.
+        //Singleton pattern is used for logging, drivers objects, caching and thread pool.
         Singleton s1 = Singleton.getInstance();
         Singleton s2 = Singleton.getInstance();
         System.out.println("s1 == s2 : " + (s1 == s2));
